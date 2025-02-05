b9bce488e44f96a8c7e44071aba3c60bbdf79b96
olsuh <olsuh@ya.ru>
2025-02-05 15:21:52 +0200

    psw

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::env;
 pub(crate) type Result<T> = std::result::Result<T, Box<dyn Error>>;
 pub(crate) type HashMap<K, V> = std::collections::HashMap<K, V>;
 
@@ -22,10 +23,10 @@ WHERE items.[$ПометкаУдаления] = 0
 
 #[ntex::main]
 async fn main() -> Result<()> {
-    let password = "<psw>>";
+    let password = env::var("DB_PSW").expect("Установите переменную среды DB_PSW");
     //let ms_connection_string = &format!("jdbc:sqlserver://localhost:1434;databaseName=ut;user=sa;password={password};");
-    //let db_url = &format!("server=127.0.0.1,1434;databaseName=ut;user=sa;password={password};TrustServerCertificate=true;");
-    //test(db_url, METADATA_FILE_NAME).await?;
+    let db_url = &format!("server=127.0.0.1,1434;databaseName=ut;user=sa;password={password};TrustServerCertificate=true;");
+    test(db_url, METADATA_FILE_NAME).await?;
     let db_url = &format!("postgres://postgres:{password}@127.0.0.1/ut"); //5432
     test(db_url, METADATA_FILE_NAME).await?;
 
